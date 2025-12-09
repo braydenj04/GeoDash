9c8cac37982564484a740d62ee49d48bf421e46c
braydenj04 <braydenjeffrey2006@gmail.com>
2025-12-09 18:50:43 +0000

    button script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+    [SerializeField] private Button playButton;
+
+    public void PlayGame()
+    {
+        if(Global.PlayMode)
+    }
+}
---
+++ Assets/Scripts/Global.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class Global : MonoBehaviour
+{
+ 
+    public static bool PlayMode = false;
+}
