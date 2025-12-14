894211b721695c72f2e03e34519faa9b8fd36ce3
braydenj04 <braydenjeffrey2006@gmail.com>
2025-12-14 20:43:38 +0000

    camera follows

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    private float SmoothSpeed = 0.125f;
+    private float StartFollow = 0;
+    public Vector3 offset;
+
+    private bool isFollowing = false;
+
+    private void Update()
+    {
+        if(!isFollowing && player != null)
+        {
+            if (player.position.x >= StartFollow) 
+            {
+                isFollowing = true;
+            }
+        }
+
+        if (isFollowing && player != null)
+        {
+            var desiredPosition = player.position + offset;
+            var smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, SmoothSpeed);
+
+            var freezeY = new Vector3(smoothedPosition.x, transform.position.y, transform.position.z);
+            transform.position = freezeY;
+        }
+    }
+}
